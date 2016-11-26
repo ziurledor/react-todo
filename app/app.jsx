761c46ddc11859842a36c86d2f853f2248c46a10
Rodel Ruiz <Rodel Ruiz>
2016-11-26 20:11:08 +0300

    Start TodoApp initial

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 
 //Load foundation
 //require('style!css!foundation-sites/dist/foundation.min.css')
@@ -11,6 +13,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );

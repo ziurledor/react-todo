6525323750c063cbf9554ccc99336025be30a7b5
Rodel Ruiz <Rodel Ruiz>
2016-11-29 22:48:49 +0300

    Add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,32 @@
+//function add(a, b) {
+//    return a + b;
+//}
+
+//console.log(add(3, 1));
+
+//var toAdd = [9, 5];
+//console.log(add(...toAdd));
+
+//var groupA = ['Jen', 'Cory'];
+//var groupB = ['Vikram'];
+//var final = [3, ...groupA];
+//var final2 = [...groupB, 3, ...groupA];
+
+//console.log(final2);
+
+var person = ['Andrew', 25];
+var personTwo = ['Jen', 29];
+
+function greet(name, age) {
+    console.log('Hi ' + name + ', you are ' + age);
+}
+
+greet(...person);
+greet(...personTwo);
+
+var names = ['Mike', 'Ben'];
+var final = ['Andrew', ...names];
+
+final.forEach(function(name) {
+    console.log('Hi ' + name);
+});

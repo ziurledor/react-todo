9ae5c7e87ccbbde56eb42085bf4cc40d7a5ab22c
Rodel Ruiz <Rodel Ruiz>
2016-12-17 22:02:30 +0300

    Refactor TodoList, Todo, and AddTodo to work with redux

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+var {connect} = require('react-redux');
 var moment = require('moment');
+var actions = require('actions');
 
-var Todo = React.createClass({
+export var Todo = React.createClass({
   render: function () {
-    var {id, text, completed, createdAt, completedAt} = this.props;
+    var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
        var message = 'Created ';
@@ -19,18 +21,19 @@ var Todo = React.createClass({
 
     return (
       <div className={todoClassName} onClick={() => {
-        this.props.onToggle(id);
+        //this.props.onToggle(id);
+        dispatch(actions.toggleTodo(id));
       }}>
       <div>
         <input type="checkbox" checked={completed}/>
       </div>
       <div>
         <p>{text}</p>
-        <p className="todo__subtext">{renderDate()}</p>      
-      </div>  
+        <p className="todo__subtext">{renderDate()}</p>
+      </div>
       </div>
     )
   }
 });
 
-module.exports = Todo;
+export default connect()(Todo);
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
@@ -19,16 +19,7 @@ var TodoApp = React.createClass({
   componentDidUpdate: function () {
     TodoAPI.setTodos(this.state.todos);
   },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-      return todo;
-    });
-    this.setState({todos: updatedTodos});
-  },
+
   handleAddTodo: function (text) {
     this.setState({
       todos: [
@@ -40,7 +31,7 @@ var TodoApp = React.createClass({
           createdAt: moment().unix(),
           completedAt: undefined
         }
-      ]  
+      ]
     });
   },
   handleSearch: function (showCompleted, searchText) {
@@ -60,7 +51,7 @@ var TodoApp = React.createClass({
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
                <TodoSearch onSearch={this.handleSearch}/>
-               <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+               <TodoList/>
                <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
           </div>

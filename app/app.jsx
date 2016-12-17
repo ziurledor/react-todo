13f8fd495407b5df0c3c83bc22e53b54bd55e491
Rodel Ruiz <Rodel Ruiz>
2016-12-17 23:54:57 +0300

    Refactor TodoSearch to work with redux store

--- app/app.jsx
+++ app/app.jsx
@@ -12,11 +12,6 @@ store.subscribe(() => {
   console.log('New state', store.getState());
 });
 
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
-
-
 //Load foundation
 //require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,9 +4,8 @@ var moment = require('moment');
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-var TodoSearch = require('TodoSearch');
+import TodoSearch from 'TodoSearch';
 var TodoAPI = require('TodoAPI');
-
 
 var TodoApp = React.createClass({
   getInitialState: function () {

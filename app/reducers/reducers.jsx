b28439f605e1252218944e4d7c78f7c9e80c50ed
Rodel Ruiz <Rodel Ruiz>
2016-12-18 22:26:47 +0300

    Refactor app to work with local storage

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -48,6 +48,12 @@ export var todosReducer = (state = [], action) => {
           return todo;
         }
       });
+    case 'ADD_TODOS':
+        return [
+          ...state,
+          ...action.todos
+        ];
+
     default:
       return state;
   }

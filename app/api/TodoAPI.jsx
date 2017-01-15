0de01895962cca2afef6845991637e80cccf3a3f
Rodel Ruiz <Rodel Ruiz>
2017-01-15 23:02:35 +0300

    Fetch todos from firebase on app start

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,25 +1,6 @@
 var $ = require('jQuery');
 
 module.exports = {
-    setTodos: function (todos) {
-        if($.isArray(todos)) {
-            localStorage.setItem('todos', JSON.stringify(todos));
-        }
-
-    },
-    getTodos: function () {
-        var stringTodos = localStorage.getItem('todos');
-        var todos = [];
-
-        try {
-          todos = JSON.parse(stringTodos);  
-        } catch (e) {
-
-        }
-
-        return $.isArray(todos) ? todos : [];
-
-    },
     filterTodos: function (todos, showCompleted, searchText) {
         var filteredTodos = todos;
 
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,47 +11,6 @@ describe('TodoAPI', () => {
       expect(TodoAPI).toExist();
    });
 
-   describe('setTodos', () => {
-       it('should set valid todos array', () => {
-           var todos = [{
-               id: 23,
-               test: 'test all files',
-               completed: false
-           }];
-           TodoAPI.setTodos(todos);
-
-           var actualTodos = JSON.parse(localStorage.getItem('todos'));
-
-           expect(actualTodos).toEqual(todos);
-       });
-
-       it('should not set invalid todos array', () => {
-           var badTodos = {a: 'b'};
-           TodoAPI.setTodos(badTodos);
-
-           expect(localStorage.getItem('todos')).toBe(null);
-       });
-   });
-
-   describe('getTodos', () => {
-       it('should return empty array for bad localstorage data', () => {
-           var actualTodos = TodoAPI.getTodos();
-           expect(actualTodos).toEqual([]);
-       });
-
-       it('should return todo if valid array in localstorage', () => {
-           var todos = [{
-               id: 23,
-               test: 'test all files',
-               completed: false
-           }];
-           localStorage.setItem('todos', JSON.stringify(todos));
-           var actualTodos = TodoAPI.getTodos();
-
-           expect(actualTodos).toEqual(todos);
-       });
-   });
-
    describe('filterTodos', () => {
        var todos = [{
            id: 1,
@@ -60,7 +19,7 @@ describe('TodoAPI', () => {
        }, {
           id: 2,
           text: 'Other text here',
-          completed: false 
+          completed: false
        }, {
           id: 3,
           text: 'Some text here',

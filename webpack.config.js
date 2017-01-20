a33e7e11691dd1afdfa67dec5153371efc834003
Rodel Ruiz <Rodel Ruiz>
2017-01-20 12:07:50 +0300

    Run webpack on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var webpack = require('webpack');
 var path = require('path');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 module.exports = {
     entry: [
         'script!jquery/dist/jquery.min.js',
@@ -14,6 +17,11 @@ module.exports = {
         new webpack.ProvidePlugin({
             '$': 'jquery',
             'jQuery': 'jquery'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compressor: {
+            warnings: false
+          }
         })
     ],
     output: {
@@ -51,5 +59,5 @@ module.exports = {
             path.resolve(__dirname, './node_modules/foundation-sites/scss')
         ]
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
